f744dc4b4ea8d385d0235197b8c1783207eaeacb
manvinder086 <manvindersingh086@gmail.com>
2025-01-21 15:09:56 +1100

    Commiting updated changes

--- data/database.js
+++ data/database.js
@@ -16,7 +16,7 @@ try {
   await client.db(dbName).command({ ping: 1 });
   console.log('Connected successfully to server');
 } catch (error) {
-  console.log('Connection.');
+  console.log('Connection Closed.');
   await client.close();
   console.log('Connection closed.');
   process.exit(1);
